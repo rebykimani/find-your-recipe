2809b304694c0f78c4a71d97517a8074f57a8c80
rebykimani <beckireby15@gmail.com>
2022-10-10 15:28:00 +0300

    passed browserRouter as route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import { BrowserRouter } from 'react';
+import { BrowserRouter as Route } from 'react-router-dom';
 
 import "../src/style.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Route>
     <App/>
-    </BrowserRouter>
+    </Route>
   </React.StrictMode>
 );
